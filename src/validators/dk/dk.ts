77e9c52ecd8975a9fde80b6c239aabc2ef8f8f81
Leonardo Lana <leolanavo@gmail.com>
2022-03-17 14:32:47 -0300

    Remove unused variables

--- src/validators/dk/dk.ts
+++ src/validators/dk/dk.ts
@@ -11,7 +11,6 @@ const DKValidator: Record<DK_STANDARDS, ValidatorConfig> = {
     format: [/[0-9]{8}/],
     validator(registration) {
       if (registration[0] === "0") return false;
-      const multipliers = [2, 7, 6, 5, 4, 3, 2, 1];
       const result: number = registration
         .split("")
         .reduce((acc, digit, index) => {
