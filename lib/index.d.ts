b115c69a570cce57e9bd75d7d545706b94a2c9e6
Leonardo Lana <leolanavo@gmail.com>
2022-03-23 14:35:50 -0300

    Export supported codes

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,3 @@
 export { CompanyIdentifiers } from "./CompanyIdentifiers";
 export { AVAILABLE_COUNTRIES_ISO_CODE } from "./enums";
+export { CODES as AVAILABLE_CODES_BY_COUNTRY } from "./validators";
