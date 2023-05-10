0c68d4d5b53c3570822db0d4c551a52401953e2e
Leonardo Lana <leolanavo@gmail.com>
2023-05-10 15:06:47 -0300

    1.5.0

--- lib/validators/index.d.ts
+++ lib/validators/index.d.ts
@@ -2,9 +2,11 @@ import { AVAILABLE_COUNTRIES_ISO_CODE } from "../enums";
 import { ValidatorConfig } from "../typings";
 import { DK_CODES } from "./dk";
 import { GB_ENG_CODES } from "./gb_eng";
+import { NO_CODES } from "./no";
 export declare const CODES: {
     DK: typeof DK_CODES;
     "GB-ENG": typeof GB_ENG_CODES;
+    NO: typeof NO_CODES;
 };
 declare type ValidatorsCollection = Record<AVAILABLE_COUNTRIES_ISO_CODE, Record<string, ValidatorConfig>>;
 declare const VALIDATORS: ValidatorsCollection;
--- src/validators/index.ts
+++ src/validators/index.ts
@@ -3,10 +3,12 @@ import { ValidatorConfig } from "../typings";
 
 import { DKValidator, DK_CODES } from "./dk";
 import { GB_ENGValidator, GB_ENG_CODES } from "./gb_eng";
+import { NOValidator, NO_CODES } from "./no";
 
 export const CODES = {
   DK: DK_CODES,
   "GB-ENG": GB_ENG_CODES,
+  NO: NO_CODES,
 };
 
 type ValidatorsCollection = Record<
@@ -17,6 +19,7 @@ type ValidatorsCollection = Record<
 const VALIDATORS: ValidatorsCollection = {
   DK: DKValidator,
   "GB-ENG": GB_ENGValidator,
+  "NO": NOValidator,
 };
 
 export { VALIDATORS };
