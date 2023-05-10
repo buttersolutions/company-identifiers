7ff93347a81419362edee4243e2541bbf3762fee
Leonardo Lana <leolanavo@gmail.com>
2023-05-10 15:06:47 -0300

    1.5.0

---
+++ lib/validators/no/no.d.ts
@@ -0,0 +1,6 @@
+import { ValidatorConfig } from "../../typings";
+declare enum NO_CODES {
+    OrgNr = "OrgNr"
+}
+declare const NOValidator: Record<NO_CODES, ValidatorConfig>;
+export { NOValidator, NO_CODES };
